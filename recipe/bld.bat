dc758bda93cbdaef3e5a998759df18bb1b72e83f
Vincent Traag <vincent@traag.net>
2020-02-13 09:44:36 +0100

    Using automake-wrapper instead

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,2 @@
-:: Make links to versioned automake
-bash "ln -s /bin/automake-1.15 /bin/automake"
-bash "ln -s /bin/aclocal-1.15 /bin/aclocal"
-
 set IGRAPH_EXTRA_CONFIGURE_ARGS="--disable-graphml"
 python -m pip install --no-deps --ignore-installed .
