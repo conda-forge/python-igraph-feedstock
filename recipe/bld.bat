c1133277e479a212cac8731d71ca375667b1bb14
Vincent Traag <vincent@traag.net>
2020-02-14 14:52:49 +0100

    Added igraphtest files. No longer printing file tree.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,5 +2,4 @@ set MSYSTEM=MINGW64
 set IGRAPH_EXTRA_CONFIGURE_ARGS="--disable-graphml"
 :: Copy missing MSVC files
 xcopy /E /Y /F %RECIPE_DIR%\msvc %SRC_DIR%\vendor\source\igraph\msvc\
-tree %SRC_DIR% /F
 python -m pip install --no-deps --ignore-installed .
