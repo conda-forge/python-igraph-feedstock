2999e11a07366b26111fb8d3d64d58568c5f7033
Vincent Traag <vincent@traag.net>
2020-02-14 11:50:15 +0100

    Add missing msvc directory

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,5 @@
 set MSYSTEM=MINGW64
 set IGRAPH_EXTRA_CONFIGURE_ARGS="--disable-graphml"
+:: Copy missing MSVC files
+xcopy /E /Y %RECIPE_DIR%\msvc %SRC_DIR%\vendor\source\igraph\msvc\
 python -m pip install --no-deps --ignore-installed .
