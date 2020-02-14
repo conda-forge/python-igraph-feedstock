a03f4dce55f6d09c9ec5fc30b18f616fb2c41925
Vincent Traag <vincent@traag.net>
2020-02-14 14:32:19 +0100

    Print out complete file tree, to clarify problem.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,4 +2,5 @@ set MSYSTEM=MINGW64
 set IGRAPH_EXTRA_CONFIGURE_ARGS="--disable-graphml"
 :: Copy missing MSVC files
 xcopy /E /Y /F %RECIPE_DIR%\msvc %SRC_DIR%\vendor\source\igraph\msvc\
+tree %SRC_DIR% /F
 python -m pip install --no-deps --ignore-installed .
