9c75f45519638af45c2410601d1e37ad5b808472
Vincent Traag <vincent@traag.net>
2021-03-26 14:35:53 +0100

    Forgot to include build script for Linux. Update libraries for Windows.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -18,6 +18,7 @@ set IGRAPH_CMAKE_EXTRA_ARGS=%CMAKE_ARGS%^
       -DIGRAPH_ENABLE_LTO=1 ^
       -DIGRAPH_ENABLE_TLS=1 ^
       -DIGRAPH_GRAPHML_SUPPORT=1
+set IGRAPH_EXTRA_LIBRARIES=libxml2,libblas,liblapack,glpk,cxsparse,gmp
 
 python -m pip install --no-deps --ignore-installed .
 if errorlevel 1 exit 1
