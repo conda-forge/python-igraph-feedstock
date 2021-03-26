6dc9853a19f09cd403dd4f66448094c606f51410
Vincent Traag <vincent@traag.net>
2021-03-26 14:03:13 +0100

    Change to CMake based build.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,23 @@
-set MSYSTEM=MINGW64
-set IGRAPH_EXTRA_CONFIGURE_ARGS="--disable-graphml"
+@echo on
+
+mkdir build
+if errorlevel 1 exit 1
+
+set IGRAPH_CMAKE_EXTRA_ARGS=%CMAKE_ARGS%^
+      -GNinja ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_PREFIX_PATH=%CONDA_PREFIX% ^
+      -DCMAKE_POSITION_INDEPENDENT_CODE=on ^
+      -DIGRAPH_USE_INTERNAL_BLAS=0 ^
+      -DIGRAPH_USE_INTERNAL_LAPACK=0 ^
+      -DIGRAPH_USE_INTERNAL_ARPACK=1 ^
+      -DIGRAPH_USE_INTERNAL_GLPK=0 ^
+      -DIGRAPH_USE_INTERNAL_CXSPARSE=0 ^
+      -DIGRAPH_USE_INTERNAL_GMP=0 ^
+      -DBUILD_SHARED_LIBS=on ^
+      -DIGRAPH_ENABLE_LTO=1 ^
+      -DIGRAPH_ENABLE_TLS=1 ^
+      -DIGRAPH_GRAPHML_SUPPORT=1
+
 python -m pip install --no-deps --ignore-installed .
+if errorlevel 1 exit 1
