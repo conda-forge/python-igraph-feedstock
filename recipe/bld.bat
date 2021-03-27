de62340121c7379a9e888c051463a2c4f3c06f04
Vincent Traag <vincent@traag.net>
2021-03-27 19:55:49 +0100

    Retry with different link order.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -14,14 +14,14 @@ set IGRAPH_CMAKE_EXTRA_ARGS=%CMAKE_ARGS%^
       -DIGRAPH_USE_INTERNAL_BLAS=0 ^
       -DIGRAPH_USE_INTERNAL_LAPACK=0 ^
       -DIGRAPH_USE_INTERNAL_ARPACK=1 ^
-      -DIGRAPH_USE_INTERNAL_GLPK=1 ^
+      -DIGRAPH_USE_INTERNAL_GLPK=0 ^
       -DIGRAPH_USE_INTERNAL_CXSPARSE=0 ^
       -DIGRAPH_USE_INTERNAL_GMP=0 ^
       -DBUILD_SHARED_LIBS=off ^
       -DIGRAPH_ENABLE_LTO=1 ^
-      -DIGRAPH_ENABLE_TLS=0 ^
+      -DIGRAPH_ENABLE_TLS=1 ^
       -DIGRAPH_GRAPHML_SUPPORT=1
-set IGRAPH_EXTRA_LIBRARIES=libxml2,blas,lapack,glpk,cxsparse,gmp
+set IGRAPH_EXTRA_LIBRARIES=gmp,blas,cxsparse,glpk,lapack,libxml2
 
 %PYTHON% -m pip install --no-deps --ignore-installed . -vvv
 if errorlevel 1 exit 1
